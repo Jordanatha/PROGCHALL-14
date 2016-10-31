39d3a8a47af61503f3dd2ace85a508cddf37a354
Jordanatha <jordanatha98@hotmail.com>
2016-10-31 07:50:41 +0700

    Finished no 15

--- PROGCHALL14 5.cpp
+++ PROGCHALL14 5.cpp
@@ -86,6 +86,9 @@ class Numdays {
 		}
 	
 		void setMaxVacation (int maxDays){
+			if (maxDays > 240){
+				cout << "Over limit. Limit = 240.";
+			}
 			maxVacation = maxDays;
 		}
 		
@@ -129,7 +132,7 @@ class Numdays {
 		
 };
 int main (){
-	int number;
+	int number, num;
 	
 	Numdays obj;
 
@@ -139,8 +142,87 @@ int main (){
 	
 	obj.setNumdays(number);
 	obj.print();
-	
+
+
+	do {
 	obj.showData();
 	
+	cout<<"1. Increase Hour(s) Worked"<<endl;
+	cout<<"2. Decrease Hour(s) Worked"<<endl;
+	cout<<"3. Input Maximum Number of Days Sick"<<endl;
+	cout<<"4. Input Number of Days Sick"<<endl;
+	cout<<"5. Input Maximum Number of Days of Paid Vacation"<<endl;
+	cout<<"6. Input Number of Days of Paid Vacation"<<endl;
+	cout<<"7. Input Maximum Number of Days of Unpaid Vacation"<<endl;
+	cout<<"8. Input Number of Days of Unpaid Vacation"<<endl;
+	cout<<"9. Show Data"<<endl;
+	cout<<"10. Exit"<<endl;	
+	cout<<"Input Choice: ";
+	cin>>num;
+	
+	switch (num){
+		case 1:
+			obj++;
+			cout << "Added to : ";
+			obj.print();
+			break;
+		
+		case 2:
+			obj--;
+			cout << "Reduced to : ";
+			obj.print();
+			break;
+			
+		case 3:
+			cout << "Input : ";
+			cin >> num;
+			obj.setMaxSickDays(num);
+			break;
+			
+		case 4:
+			cout << "Input : ";
+			cin >> num;
+			obj.setSickTaken (num);
+			break;
+			
+		case 5:
+			cout << "Input : ";
+			cin >> num;
+			obj.setMaxVacation(num);
+			break;
+			
+		case 6:
+			cout << "Input : ";
+			cin >> num;
+			obj.setVacTaken (num);
+			break;
+		
+		case 7:
+			cout << "Input : ";
+			cin >> num;
+			obj.setMaxUnpaid(num);
+			break;
+			
+		case 8:
+			cout << "Input : ";
+			cin >> num;
+			obj.setUnpaidTaken(num);
+			break;
+			
+		case 9:
+			obj.showData();
+			break;
+		
+		case 10:
+				exit (0);
+	}
+	cout << "Press Enter to Continue.";
+	cin.ignore();
+	cin.get();
+	
+	cout << string (20, '\n');
+}
+	while (num != 10);
+
 	return 0;
 }
